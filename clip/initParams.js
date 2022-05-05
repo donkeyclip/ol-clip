45852dca4f0aea9431fab5009b68fc96599336e6
Elisavet <68596273+ElisavetAmpatzidou@users.noreply.github.com>
2022-05-06 01:15:37 +0300

    fix: update initParams

--- clip/initParams.js
+++ clip/initParams.js
@@ -3,7 +3,7 @@ export const initParams = [
     name: "Default",
     value: {
       text: ["Welcome","To The","motorcortex","open layers","plugin"],
-      description:["in this short","clip we","will show","some of the ","capabilityes","of our library"],
+      description:["in this short","clip we","will show","some of the ","capabilities","of our library"],
      
     },
   },
